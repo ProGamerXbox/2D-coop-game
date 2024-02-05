66cb2535b0543cf6b772237c84096367ca5a460f
dannim272 <dmorozkin26@gmail.com>
2024-02-05 11:51:54 +0000

    wasd
    wasd

--- sourcecode/client/ProjAlpha.py
+++ sourcecode/client/ProjAlpha.py
@@ -31,11 +31,7 @@ PLAYER1_JUMPING_IMAGE = pygame.image.load('img/player_running.png')
 PLAYER1_JUMPING = pygame.transform.flip(pygame.transform.scale(PLAYER1_JUMPING_IMAGE, (PLAYER_WIDTH,PLAYER_HEIGHT)), True, False)
 PLAYER1_RUNNING_IMAGE = pygame.image.load('img/player_running1.png')
 PLAYER1_RUNNING = pygame.transform.flip(pygame.transform.scale(PLAYER1_RUNNING_IMAGE, (PLAYER_WIDTH,PLAYER_HEIGHT)), True, False)
-
-
 # PLAYER 2
-PLAYER2_IMAGE = pygame.image.load('img/player_shooting.png')
-PLAYER_2 = pygame.transform.flip(pygame.transform.scale(PLAYER2_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT)), True, False)
 PlAYER2_JUMPING_IMAGE = pygame.image.load('img/player_running.png')
 PlAYER2_JUMPING = pygame.transform.scale(PlAYER2_JUMPING_IMAGE, (PLAYER_WIDTH,PLAYER_HEIGHT))
 PLAYER2_RUNNING_IMAGE = pygame.image.load('img/player_running1.png')
@@ -43,11 +39,8 @@ PLAYER2_RUNNING = pygame.transform.scale(PLAYER2_RUNNING_IMAGE, (PLAYER_WIDTH,PL
 
 
 # MOVEMENT CONFIG
-walkRight = [PLAYER_1]
-walkLeft = [PLAYER_2]
 VEL = 5
 walkCount1 = 0
-walkCount2 = 0
 
 
 # JUMP SETTINGS
@@ -55,13 +48,10 @@ isJump = False
 jumpCount = 10
 
 
-def draw_window(p1, p2):
+def draw_window(p1):
     from Player1_movement import left1
     from Player1_movement import right1
     from Player1_movement import walkCount1
-    from Player2_movement import left2
-    from  Player2_movement import right2
-    from Player2_movement import walkCount2
     WIN.blit(BACKGROUND, (0, 0))
     WIN.blit(PLATFORM_1, (150,728))
 # 400, 1250
@@ -77,18 +67,6 @@ def draw_window(p1, p2):
     else:
         WIN.blit(PLAYER_1, (p1.x, p1.y))
 
-    if walkCount2 + 1 >= 27:
-        walkCount2 = 0
-
-    if left2:
-        WIN.blit(PLAYER1_RUNNING, (p2.x, p2.y + 18))
-        walkCount2 += 1
-    elif right2:
-        WIN.blit(PLAYER2_RUNNING, (p2.x, p2.y + 18))
-        walkCount2 += 1
-    else:
-        WIN.blit(PLAYER_2, (p2.x, p2.y))
-
     pygame.display.update()
 
 def main():
@@ -96,8 +74,7 @@ def main():
     global right
     global left
     global jumping, VELOCITY, isJump, jumpCount
-    p1 = pygame.Rect(150, 615, PLAYER_WIDTH, PLAYER_HEIGHT) # RIGHT PLAYER
-    p2 = pygame.Rect(1150, 589, PLAYER_WIDTH, PLAYER_HEIGHT) # LEFT PLAYER
+    p1 = pygame.Rect(150, 615, PLAYER_WIDTH, PLAYER_HEIGHT)
     clock = pygame.time.Clock()
     run = True
     
@@ -109,33 +86,13 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         player_1_movement(keys_pressed, p1)
-        player_2_movement(keys_pressed, p2)
         player1_jumping(keys_pressed, p1)
-        player2_jumping(keys_pressed, p2)
 
-        draw_window(p1, p2)
-
-        def Player1_jump(keys_pressed):
-
-            isJump = False
-            jumpCount = 10
-
-            if not (isJump):
-                if keys_pressed[pygame.K_w]:
-                    isJump = True
-            else:
-                if jumpCount >= -10:
-                    neg = 1
-                    if jumpCount < 0:
-                        neg = -1
-                    p1.y -= (jumpCount ** 2) * 0.5 * neg
-                    jumpCount -= 1
-                else:
-                    isJump = False
-                    jumpCount = 10
+        draw_window(p1)
 
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
--- sourcecode/client/resolution.py
+++ sourcecode/client/resolution.py
@@ -1,7 +1,16 @@
 import pygame
 
 def res():
-
+    print("Here are some common examples of 'square' resolutions:")
+    print("""
+                     '4:3'    |     '16:9'  
+                - 1600 x 1200 | 1920 x 1080 -
+                - 1280 x 960  |  1600 x 900 -
+                - 1152 x 864  |  1280 x 720 -
+                - 1024 x 768  |  1024 x 576 -
+                - 800 x 600   |   960 x 540 -
+                ------------- | ------------- 
+""")
     WIDTH = int(input("What width would you like to play in?: "))
     while WIDTH < 250:
         WIDTH = int(input("The width you've entered is too small, enter another width: "))
@@ -17,18 +26,13 @@ def res():
         print("Resolution parameters you've entered are not 'Square'.")
         print("Here are some common examples of 'square' resolutions:")
         print("""
-        '4:3':
-        - 1600 x 1200
-        - 1280 x 960
-        - 1152 x 864
-        - 1024 x 768
-        - 800 x 600
-        -------------
-        '16:9':
-        - 1920 x 1080
-        - 1600 x 900
-        - 1280 x 720
-        - 960 x 540
+             '4:3'    |     '16:9'  
+        - 1600 x 1200 | 1920 x 1080 -
+        - 1280 x 960  |  1600 x 900 -
+        - 1152 x 864  |  1280 x 720 -
+        - 1024 x 768  |  1024 x 576 -
+        - 800 x 600   |   960 x 540 -
+        ------------- | ------------- 
 """)
 
         WIDTH = int(input("What width would you like to play in?: "))
