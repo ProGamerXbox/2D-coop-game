1e5e4abc7168152a89d152cc6073f381c851b544
dannim272 <dmorozkin26@gmail.com>
2024-02-07 20:08:31 +0000

    wasd
    wasd

--- sourcecode/client/Player_movement.py
+++ sourcecode/client/Player_movement.py
@@ -1,14 +1,13 @@
 import pygame
 
-
-WIDTH, HEIGHT = 1600, 900
-
-# MOVEMENT SETTINGS
+# SETTINGS
 VEL = 150
 left1 = False
 right1 = False
 walkCount1 = 0
 PLAYER_WIDTH, PLAYER_HEIGHT = 136, 115
+WIDTH = 1920
+
 
 
 # MOVEMENT
@@ -29,19 +28,18 @@ def player_movement(keys_pressed, p1, delta_time):
 isJump1 = False
 jumpCount1 = 10
 
-def player_jumping(keys_pressed, p1):
-    ################
+def player_jumping(keys_pressed, p1, delta_time):
     global isJump1
     global jumpCount1
     if not (isJump1):
-        if keys_pressed[pygame.K_w]:
+        if keys_pressed[pygame.K_SPACE]:
             isJump1 = True
     else:
         if jumpCount1 >= -10:
             neg = 1
             if jumpCount1 < 0:
                 neg = -1
-            p1.y -= (jumpCount1 ** 2) * neg
+            p1.y -= (jumpCount1 ** 2) * neg * (delta_time*10)
             jumpCount1 -= 1
         else:
             isJump1 = False
